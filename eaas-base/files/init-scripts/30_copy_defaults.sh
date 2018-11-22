8f04fd869a05ee7ea9ee16fc0818fa395ea5e317
Klaus Rechert <klaus@openslx.com>
2018-11-22 21:30:31 +0100

    defaults / nginx / siefried

--- eaas-base/files/init-scripts/30_copy_defaults.sh
+++ eaas-base/files/init-scripts/30_copy_defaults.sh
@@ -3,3 +3,13 @@
 if [ ! -e /home/bwfla/server-data/operating-systems.json ]; then
    cp -v /home/bwfla/defaults/operating-systems.json /home/bwfla/server-data
 fi
+
+if [ -e /home/bwfla/defaults/templates ]; then
+   mkdir -p /home/bwfla/image-archive/meta-data/template
+   cp -vr /home/bwfla/defaults/templates/* /home/bwfla/image-archive/meta-data/template
+fi
+
+if [ -e /home/bwfla/defaults/remote ]; then
+   mkdir -p /home/bwfla/image-archive/remote/meta-data/base/
+   cp -rv /home/bwfla/defaults/remote/linux/* /home/bwfla/image-archive/remote/meta-data/base/
+fi
